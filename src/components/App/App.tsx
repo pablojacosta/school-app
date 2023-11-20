48faec484745819c2d8ee74de7db01b0bde8bcb3
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-11-20 16:24:53 -0300

    feat: :wrench: Configured paths. Added global styles.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import "@styles/globals.scss";
 import styles from "./App.module.scss";
 
 const App = () => {
   return (
     <div className={styles.app}>
       <h1>School App</h1>
+      <p>TEST TEXT</p>
     </div>
   );
 };
