fb84aa736a8ce16108ec7aeef58d4bb0fda53ff0
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-11-20 16:59:33 -0300

    feat: :sparkles: Configured react-router-dom.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,18 @@
 import "@styles/globals.scss";
 import styles from "./App.module.scss";
+import { Routes, Route } from "react-router-dom";
 import RoomsData from "@components/RoomsData";
 import StudentsData from "@components/StudentsData";
+import { HOME, ROOMS, STUDENTS } from "constants/routes";
 
 const App = () => {
   return (
     <div className={styles.app}>
-      <h1>School App</h1>
-      <RoomsData />
-      <StudentsData />
+      <Routes>
+        <Route path={HOME} element={<h1>School App</h1>} />
+        <Route path={ROOMS} element={<RoomsData />} />
+        <Route path={STUDENTS} element={<StudentsData />} />
+      </Routes>
     </div>
   );
 };
