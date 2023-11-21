a8a60bba0eac153416a2e348d5868c195e792f7e
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-11-21 20:47:43 -0300

    feat: :sparkles: Added spinner to StudentsData component.

--- src/components/pages/StudentsData/StudentsData.tsx
+++ src/components/pages/StudentsData/StudentsData.tsx
@@ -1,7 +1,8 @@
-import styles from "./StudentsData.module.scss";
+import { TailSpin } from "react-loader-spinner";
 import StudentsTable from "../../shared/StudentsTable";
 import CreateButton from "@components/shared/CreateButton";
 import useGetStudents from "hooks/students/useGetStudents";
+import styles from "./StudentsData.module.scss";
 
 const StudentsData = () => {
   const { students, studentsError, studentsIsLoading } = useGetStudents();
@@ -13,6 +14,11 @@ const StudentsData = () => {
       <h1>Students</h1>
       <CreateButton isRoom={false} />
       {showStudentsTable && <StudentsTable studentsData={students} showRoom />}
+      {studentsIsLoading && (
+        <div className={styles.spinner}>
+          <TailSpin />
+        </div>
+      )}
     </div>
   );
 };
