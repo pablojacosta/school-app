9588e6dc3c18f67fc394bbc69f566cfe9ffe285a
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-11-21 08:40:29 -0300

    feat: :sparkles: Created RoomCreate form.

--- src/components/RoomsData/components/RoomCreate/RoomCreate.tsx
+++ src/components/RoomsData/components/RoomCreate/RoomCreate.tsx
@@ -1,9 +1,108 @@
+import { Field, Form } from "react-final-form";
 import styles from "./RoomCreate.module.scss";
 
 const RoomCreate = () => {
+  const onSubmit = () => {};
+
   return (
     <div className={styles.roomCreate}>
       <h1>Create Room</h1>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.field}>
+              <label className={styles.label}>Room's Name</label>
+              <Field
+                name="roomName"
+                component={"input"}
+                type="text"
+                placeholder="Room's name"
+              />
+            </div>
+            <div className={styles.field}>
+              <label className={styles.label}>Subject</label>
+              <div className={styles.radio}>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="math"
+                  />{" "}
+                  Math
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="literature"
+                  />{" "}
+                  Literature
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="geography"
+                  />{" "}
+                  Geography
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="biology"
+                  />{" "}
+                  Biology
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="history"
+                  />{" "}
+                  History
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="music"
+                  />{" "}
+                  Music
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="technology"
+                  />{" "}
+                  Technology
+                </label>
+                <label>
+                  <Field
+                    name="subject"
+                    component="input"
+                    type="radio"
+                    value="other"
+                  />{" "}
+                  Other
+                </label>
+              </div>
+            </div>
+            <button type="submit" className={styles.submitButton}>
+              Submit
+            </button>
+          </form>
+        )}
+      ></Form>
     </div>
   );
 };
