6bd0992983796c7048cbd1a502becad947f5af05
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-11-22 22:52:24 -0300

    feat: :iphone: Added mobile responsives.

--- src/components/pages/StudentsData/components/AddStudent/AddStudent.tsx
+++ src/components/pages/StudentsData/components/AddStudent/AddStudent.tsx
@@ -46,121 +46,131 @@ const AddStudent = () => {
 
   return (
     <>
-      {roomsError && <ErrorMessage />}
-      {roomsIsLoading && <Spinner />}
-      {rooms && (
-        <div className={styles.addStudent}>
-          <h1>Add Student</h1>
-          <Form onSubmit={onSubmit}>
-            {({ handleSubmit }) => (
-              <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.field}>
-                  <Field name="firstName" validate={required}>
-                    {({ input, meta }) => (
-                      <>
-                        <label className={styles.label}>First Name</label>
-                        <input
-                          {...input}
-                          type="text"
-                          placeholder="First name"
-                        />
-                        {meta.error && meta.touched && (
-                          <span>{meta.error}</span>
-                        )}
-                      </>
-                    )}
-                  </Field>
-                </div>
-                <div className={styles.field}>
-                  <Field name="lastName" validate={required}>
-                    {({ input, meta }) => (
-                      <>
-                        <label className={styles.label}>Last Name</label>
-                        <input {...input} type="text" placeholder="Last name" />
-                        {meta.error && meta.touched && (
-                          <span>{meta.error}</span>
-                        )}
-                      </>
-                    )}
-                  </Field>
-                </div>
-                <div className={styles.selectField}>
-                  <label className={styles.label}>Age</label>
-                  <Field
-                    name="age"
-                    component={SelectAdapter}
-                    validate={required}
-                    options={ageOptions}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <label className={styles.label}>Gender</label>
-                  <div className={styles.radio}>
-                    <label>
-                      <Field
-                        name="gender"
-                        component="input"
-                        type="radio"
-                        value="rather not say"
-                        initialValue="rather not say"
-                      />{" "}
-                      Rather not say
-                    </label>
-                    <label>
-                      <Field
-                        name="gender"
-                        component="input"
-                        type="radio"
-                        value="non-binary"
-                      />{" "}
-                      Non-Binary
-                    </label>
-                    <label>
-                      <Field
-                        name="gender"
-                        component="input"
-                        type="radio"
-                        value="male"
-                      />{" "}
-                      Male
-                    </label>
-                    <label>
-                      <Field
-                        name="gender"
-                        component="input"
-                        type="radio"
-                        value="female"
-                      />{" "}
-                      Female
-                    </label>
+      <div className={styles.addStudent}>
+        {(roomsError || roomsIsLoading) && (
+          <div className={styles.loadingOrError}>
+            {roomsError && <ErrorMessage />}
+            {roomsIsLoading && <Spinner />}
+          </div>
+        )}
+        {rooms && (
+          <>
+            <h1>Add Student</h1>
+            <Form onSubmit={onSubmit}>
+              {({ handleSubmit }) => (
+                <form onSubmit={handleSubmit} className={styles.form}>
+                  <div className={styles.field}>
+                    <Field name="firstName" validate={required}>
+                      {({ input, meta }) => (
+                        <>
+                          <label className={styles.label}>First Name</label>
+                          <input
+                            {...input}
+                            type="text"
+                            placeholder="First name"
+                          />
+                          {meta.error && meta.touched && (
+                            <span>{meta.error}</span>
+                          )}
+                        </>
+                      )}
+                    </Field>
                   </div>
-                </div>
-                <div className={styles.selectField}>
-                  <label className={styles.label}>Room</label>
-                  <Field
-                    name="room"
-                    component={SelectAdapter}
-                    validate={required}
-                    options={rooms.map((room) => ({
-                      label: room.name,
-                      value: room.name,
-                    }))}
-                  />
-                </div>
-                <button type="submit" className={styles.submitButton}>
-                  Submit
-                </button>
-                {studentIsLoading && (
-                  <span className={styles.spinner}>
-                    <TailSpin height="20" width="20" />
-                  </span>
-                )}
-              </form>
-            )}
-          </Form>
-          <GoBackButton />
-        </div>
-      )}
+                  <div className={styles.field}>
+                    <Field name="lastName" validate={required}>
+                      {({ input, meta }) => (
+                        <>
+                          <label className={styles.label}>Last Name</label>
+                          <input
+                            {...input}
+                            type="text"
+                            placeholder="Last name"
+                          />
+                          {meta.error && meta.touched && (
+                            <span>{meta.error}</span>
+                          )}
+                        </>
+                      )}
+                    </Field>
+                  </div>
+                  <div className={styles.selectField}>
+                    <label className={styles.label}>Age</label>
+                    <Field
+                      name="age"
+                      component={SelectAdapter}
+                      validate={required}
+                      options={ageOptions}
+                    />
+                  </div>
+                  <div className={styles.field}>
+                    <label className={styles.label}>Gender</label>
+                    <div className={styles.radio}>
+                      <label>
+                        <Field
+                          name="gender"
+                          component="input"
+                          type="radio"
+                          value="rather not say"
+                          initialValue="rather not say"
+                        />{" "}
+                        Rather not say
+                      </label>
+                      <label>
+                        <Field
+                          name="gender"
+                          component="input"
+                          type="radio"
+                          value="non-binary"
+                        />{" "}
+                        Non-Binary
+                      </label>
+                      <label>
+                        <Field
+                          name="gender"
+                          component="input"
+                          type="radio"
+                          value="male"
+                        />{" "}
+                        Male
+                      </label>
+                      <label>
+                        <Field
+                          name="gender"
+                          component="input"
+                          type="radio"
+                          value="female"
+                        />{" "}
+                        Female
+                      </label>
+                    </div>
+                  </div>
+                  <div className={styles.selectField}>
+                    <label className={styles.label}>Room</label>
+                    <Field
+                      name="room"
+                      component={SelectAdapter}
+                      validate={required}
+                      options={rooms.map((room) => ({
+                        label: room.name,
+                        value: room.name,
+                      }))}
+                    />
+                  </div>
+                  <button type="submit" className={styles.submitButton}>
+                    Submit
+                  </button>
+                  {studentIsLoading && (
+                    <span className={styles.spinner}>
+                      <TailSpin height="20" width="20" />
+                    </span>
+                  )}
+                </form>
+              )}
+            </Form>
+            <GoBackButton />
+          </>
+        )}
+      </div>
     </>
   );
 };
